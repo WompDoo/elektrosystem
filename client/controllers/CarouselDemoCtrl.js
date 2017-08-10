8a73576a9176eec500f6c8a4ccb85f411e3cdcf8
Heigo Kure <heigo.kure@khk.ee>
2017-08-10 12:13:33 +0300

    A lot of small changes and fixes

--- client/controllers/CarouselDemoCtrl.js
+++ client/controllers/CarouselDemoCtrl.js
@@ -3,31 +3,28 @@ CarouselDemoCtrl.controller('CarouselDemoCtrl', function CarouselDemoCtrl($scope
 
     $scope.slides = [
         {
+            image: 'img/Embach.svg', link: 'http://www.embach.ee'
+        },
+        {
+            image: 'img/Nordecon.png', link: 'http://www.nordecon.com'
+        },
+        {
+            image: 'img/Yit.png', link: 'https://www.yit.ee/avaleht'
+        },
+        {
+            image: 'img/Ehitustrust.png', link: 'http://ehitustrust.ee'
+        },
+        {
+            image: 'img/KRC.png', link: 'http://www.krc.ee'
+        },
+        {
             image: 'img/deckol.png', link: 'http://www.deckol.ee'
         },
         {
             image: 'img/Ramm.png', link: 'https://www.rammehitus.ee'
         },
         {
-            image: 'img/Ehitustrust.png', link: 'http://ehitustrust.ee'
-        },
-        {
-            image: 'img/Embach.svg', link: 'http://www.embach.ee'
-        },
-        {
             image: 'img/Eston.png', link: 'http://eston.nordecon.com'
-        },
-        {
-            image: 'img/Fausto.png', link: 'http://www.fausto.ee/et/'
-        },
-        {
-            image: 'img/KRC.png', link: 'http://www.krc.ee'
-        },
-        {
-            image: 'img/Nordecon.png', link: 'http://www.nordecon.com'
-        },
-        {
-            image: 'img/Mapri.png', link: 'http://mapri.eu'
         },
         {
             image: 'img/OmaEhitaja.png', link: 'http://www.oma.ee'
@@ -36,7 +33,10 @@ CarouselDemoCtrl.controller('CarouselDemoCtrl', function CarouselDemoCtrl($scope
             image: 'img/Trekwerk.png', link: 'http://www.trekwerk.com'
         },
         {
-            image: 'img/Yit.png', link: 'https://www.yit.ee/avaleht'
+            image: 'img/Mapri.png', link: 'http://mapri.eu'
+        },
+        {
+            image: 'img/Fausto.png', link: 'http://www.fausto.ee/et/'
         }
 
     ];
